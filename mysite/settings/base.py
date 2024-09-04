97b12dd582fa2727fe551a9b0dc7dff76605a5ee
rodolfo150194 <rodolfogarciamaeso@gmail.com>
2024-09-04 12:10:58 -0400

    configurando para deploy

--- mysite/settings/base.py
+++ mysite/settings/base.py
@@ -20,6 +20,7 @@ BASE_DIR = os.path.dirname(PROJECT_DIR)
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
+ALLOWED_HOSTS = [".vercel.app"]
 
 # Application definition
 
