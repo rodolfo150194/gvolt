6499d4d30787eb49fb3505588676f9d5835bab32
rodolfo <manuel@code43w.net>
2024-09-25 00:17:56 -0400

    cambiando bd a postgres

--- mysite/settings/base.py
+++ mysite/settings/base.py
@@ -92,18 +92,18 @@ WSGI_APPLICATION = "mysite.wsgi.application"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-    },
     # "default": {
-    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    #     'NAME': 'verceldb',
-    #     'USER': 'default',
-    #     'PASSWORD': 'ot0re8LlshYB',
-    #     'HOST': 'ep-falling-cell-a4xqgpk6-pooler.us-east-1.aws.neon.tech',
-    #     'PORT': '5432',
-    # }
+    #     "ENGINE": "django.db.backends.sqlite3",
+    #     "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+    # },
+    "default": {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'verceldb',
+        'USER': 'default',
+        'PASSWORD': 'ot0re8LlshYB',
+        'HOST': 'ep-falling-cell-a4xqgpk6-pooler.us-east-1.aws.neon.tech',
+        'PORT': '5432',
+    }
 }
 
 # Password validation
